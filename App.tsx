36c74eb0230743813e167a48740b4989b833f491
julianamo93 <julinnamor93@gmail.com>
2024-09-11 21:31:22 -0300

    Projeto base - Turma 2TDSZ

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá Mundo do 2TDSZ da FIAP Lins!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
